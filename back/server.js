088aa24de81bc0d7766211ea7259478ea8439a71
Gabriel Silva <86989966+Gajsilva@users.noreply.github.com>
2022-07-04 20:28:41 -0300

    continuando com backend

--- back/server.js
+++ back/server.js
@@ -4,20 +4,9 @@ http
 .createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'application/json' });
 
-    if(request.url === 'produto') {
-        response.end(JSON.stringify({
-            message: "Rota de produto"
-        }))
-    }
+    if(request.url === "/produto");
 
-    if(request.url === 'usuario') {
-        response.end(JSON.stringify({
-            message: "Rota de usuario"
-        }))
-    }
+    if(request.url === "/usuario");
     
-    response.end(JSON.stringify({
-        message:"minha primeira aplicação node"
-    }));
 })
 .listen(4001, () => console.log("servidor na porta 4001"))
